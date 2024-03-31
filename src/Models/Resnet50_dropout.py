0d1577f96ea810f74a2f95673d36d114a52152ab
atpapadop <atpapadop@it.auth.gr>
2024-03-31 20:15:57 +0300

    changed dropout layer chance to 0.8 on resnet and efficientnet

--- src/Models/Resnet50_dropout.py
+++ src/Models/Resnet50_dropout.py
@@ -7,4 +7,4 @@ import torch
 import torchvision
 
 model = torchvision.models.resnet50(weights = torchvision.models.ResNet50_Weights.IMAGENET1K_V2)
-model.fc = torch.nn.Sequential(torch.nn.Dropout(p=0.5), torch.nn.Linear(2048,23))
+model.fc = torch.nn.Sequential(torch.nn.Dropout(p=0.8), torch.nn.Linear(2048,23))
